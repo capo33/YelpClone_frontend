1e6ce794f9d0b9d5e5c5654983762b0b6bc88b2a
Mohamed <s2000588@edu.bc.fi>
2023-05-05 10:59:33 +0300

    update baseURL

--- src/Apis/baseUrl.ts
+++ src/Apis/baseUrl.ts
@@ -1,5 +1,15 @@
 import axios from "axios";
 
+// NODE_ENV = "production" or "development"
+
+// if we are in production baseURl = "/api/v1/restaurants"
+// else baseURl = "http://localhost:8080/api/v1/restaurants"
+
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "/api/v1/restaurants"
+    : "https://yelpclone-r2uw.onrender.com/api/v1/restaurants";
+
 export default axios.create({
-  baseURL: "http://localhost:8080/api/v1/restaurants",
+  baseURL,
 });
